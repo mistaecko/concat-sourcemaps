83c03c78873a32b8b1dec29bc45ff0d1e3e08077
mistaecko <mistaecko@gmail.com>
2013-10-08 18:15:59 +0700

    use grunt.file.mkdir instead of mkdirp

--- tasks/concat-sourcemaps.js
+++ tasks/concat-sourcemaps.js
@@ -1,18 +1,16 @@
 "use strict";
 
-var mkdirp = require("mkdirp");
 var path = require("path");
 var concat = require("../src/concat-sourcemaps.js").concat;
 
 module.exports = function(grunt) {
 
     grunt.registerMultiTask("concat-sourcemaps", "Combines Source Map files while concatenating JavaScript source files.", function() {
-        var options = this.options({});
         var files = this.files;
 
         // Iterate over all specified file groups.
         files.forEach(function (f) {
-            mkdirp.sync(path.dirname(f.dest));
+            grunt.file.mkdir(path.dirname(f.dest));
             concat(f.src, f.dest);
         });
     });
